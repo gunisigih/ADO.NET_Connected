b69cad12f26af0e7fd20daa4b0aa6cd6db6f5825
gunisigih <46692137+gunisigih@users.noreply.github.com>
2019-02-28 00:06:29 +0300

    first commit

---
+++ NT_ADO.Net_Son_Proje/Tools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NT_ADO.Net_Son_Proje
+{
+    class Tools
+    {
+        public static string Connectionstring
+        {
+            get
+            {
+                return @"Data Source=.\SQLEXPRESS; Initial Catalog=Northwind;Integrated Security=True";
+            }
+            
+        }
+    }
+}
